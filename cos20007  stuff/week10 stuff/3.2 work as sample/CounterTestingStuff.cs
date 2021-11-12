53b0a31b5ce1120f1d5b89c867966f177b4e167f
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 16:34:38 +0600

    Add files via upload

---
+++ cos20007  stuff/week10 stuff/3.2 work as sample/CounterTestingStuff.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterTest;
+using NUnit.Framework;
+
+
+namespace TestCounterAndClock
+{
+    class CounterTestingStuff
+    {
+        Counter _counter = new Counter("tester");
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _counter.Reset();
+        }
+
+        [Test]
+        public void StartTime()
+        {
+            //define expected result
+            int expected = 0;
+
+            //run the test
+            
+
+            //define actual result
+            int actual = _counter.Ticks;
+
+            // Assert expected and actual are the same
+
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IncrementBy1()
+        {
+            //define expected result
+            int expected = 1;
+
+            //run the test
+            _counter.Increment();
+
+            //define actual result
+            int actual = _counter.Ticks;
+
+            // Assert expected and actual are the same
+
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IncrementMultiple()
+        {
+            //define expected result
+            int expected = 10;
+
+            //run the test
+            for(int i =0; i<10;i++)
+            {
+                _counter.Increment();
+            }
+
+            //define actual result
+            int actual = _counter.Ticks;
+
+            // Assert expected and actual are the same
+
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ResetTime()
+        {
+            //define expected result
+            int expected = 0;
+
+            //run the test
+            for (int i = 0; i < 10; i++)
+            {
+                _counter.Increment();
+            }
+            _counter.Reset();
+
+            //define actual result
+            int actual = _counter.Ticks;
+
+            // Assert expected and actual are the same
+
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
