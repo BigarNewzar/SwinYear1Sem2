f91de87125f2ffaa8dabd748e0715f47f52d9edd
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 16:47:21 +0600

    Add files via upload

---
+++ cos20007  stuff/week 9 stuff/SwinAdventureGUI/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace SwinAdventureGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(41, 307);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(402, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(552, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(41, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(623, 201);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SwinAdventureGUI";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ cos20007  stuff/week 9 stuff/SwinAdventureGUI/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Swin_Adventure;
+
+namespace SwinAdventureGUI
+{
+    public partial class Form1 : Form
+    {
+        private string _name, _desc;
+        private Player _player;
+        private Item _sword, _torch, _orb;
+        private Bag _bag;
+        private Location _smallcloset,_hallway;
+        private Path _hallway_path;
+       
+        private CommandProcessor command;
+        private string _inputText;
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = "Welcome to Swin Adventure! \r\n You have arrived in the Hallway ";
+
+            _name = "Player";
+            _desc = "someone who is playing the game";
+
+
+           _player = new Player(_name, _desc);
+
+            _sword = new Item(new string[] { "sword" }, "sword", "an old rusty sword");
+
+            _torch = new Item(new string[] { "torch" }, "torch", "a torch to light the way");
+
+            _player.Inventory.Put(_sword);
+
+            _player.Inventory.Put(_torch);
+
+
+            _bag = new Bag(new string[] { "bag" }, "bagpack", "initial bagpack for player");
+
+            _player.Inventory.Put(_bag);
+
+
+
+            _orb = new Item(new string[] { "orb" }, "shiny orb", "a mysterious orb that seems to glow from within");
+
+            _bag.Inventory.Put(_orb);
+
+
+
+
+            _smallcloset = new Location(new String[] { "small closet", "smallcloset" }, "small closet",
+                                                    "A small dark closet, with an odd smell");
+
+            _hallway_path = new Path(new String[] { "South" }, _smallcloset,
+                                        "hallway_path", "You go through a door.");
+            _hallway = new Location(new String[] { "hallway", "long hallway" }, "hallway",
+                                            "This is a long well lit hallway");
+
+            _player.Location = _hallway;
+
+            _hallway.PutPath(_hallway_path);
+            _hallway.GetPathIndividual(_hallway_path.FirstID);//not sure if this is really needed
+
+            command = new CommandProcessor();
+
+          
+
+        }
+
+        string[] StringToSubstring(string text)
+        {
+            char delimiterChar = ' ';// space will be used to seperate them
+            return text.Split(delimiterChar);// split will split the string into substrings
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _inputText = textBox2.Text;
+            textBox1.Text = command.Execute(_player, StringToSubstring(_inputText)).Replace("\n", "\r\n");//basically, replace will find all instances of first string and replace with the 2nd string. Thus form will understand that \n means \r\n which is new line.
+
+
+            
+            if(_inputText == "quit")//once stored string is quit, after running the command (as it is outside else if part), it will kill the code 
+            {
+                System.Environment.Exit(0);
+            }
+            
+
+            
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
