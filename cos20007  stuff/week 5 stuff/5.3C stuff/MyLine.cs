d46d822f5801b57bd801aacaba49942a05b57df9
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 17:13:25 +0600

    Add files via upload

---
+++ cos20007  stuff/week 5 stuff/5.3C stuff/MyLine.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class MyLine: Shape
+    {
+        private float _endX;
+        private float _endY;
+
+        public MyLine(Color Clr, float startX, float startY, float endX, float endY)
+        {
+
+            Color = Clr;
+            X = startX;
+            Y = startY;
+            _endX = endX;
+            _endY = endY;
+
+
+        }
+        public MyLine() : this(Color.Red, 0, 0, 0, 0)
+        {
+        }
+
+        public float EndX
+        {
+            get
+            {
+                return _endX;
+            }
+            set
+            {
+                _endX = value;
+            }
+        }
+
+        public float EndY
+        {
+            get
+            {
+                return _endY;
+            }
+            set
+            {
+                _endY = value;
+            }
+        }
+        public override void Draw()
+        {
+            SplashKit.DrawLine(Color, X, Y, EndX,EndY);
+
+            if (selected)
+            {
+                DrawOutline();
+
+            }
+        }
+        public override void DrawOutline()
+        {
+            SplashKit.DrawCircle(Color.Black, x: X, y: Y, 2
+                );//cricle outline at start of line
+            SplashKit.DrawCircle(Color.Black, x: EndX, y: EndY, 2
+                );//cricle outline at end of line
+            SplashKit.DrawRectangle(Color.Black, x: X + 2, y: Y + 2,(EndX-X) + 4, (EndY - Y) + 4
+                ); //rectangle outline for the straight part
+        }
+        public override Boolean IsAt(Point2D pt)
+        {
+            if (SplashKit.PointOnLine(pt, SplashKit.LineFrom(X, Y, EndX,EndY)))
+            {
+                return true;
+
+            }
+            else
+                return false;
+        }
+
+        public override void SaveTo(StreamWriter writer)
+        {
+            //writer.WriteLine("Line");
+            base.SaveTo(writer);
+
+            writer.WriteLine(EndX);
+            writer.WriteLine(EndY);
+        }
+        public override void LoadFrom(StreamReader reader)
+        {
+            base.LoadFrom(reader);
+
+            EndX = reader.ReadInteger();
+            EndY = reader.ReadInteger();
+
+        }
+    }
+}
---
+++ cos20007  stuff/week 5 stuff/5.3C stuff/Shape.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplashKitSDK;
+using System.IO;
+
+namespace ShapeDrawer
+{
+    public abstract class Shape
+    {
+
+        private static Dictionary<string, Type> _ShapeClassRegistry = new Dictionary<string, Type>();
+
+        private Color _color;
+        private float _x, _y;
+        
+        private Boolean _selected;
+
+        public Shape() : this (Color.Yellow)
+        {
+        }
+
+        public Shape(Color color) 
+        {
+            _color = color;
+            
+
+        }
+
+        public static void RegisterShape(string name, Type t)
+        {
+            _ShapeClassRegistry[name] = t;
+        }
+
+        //output string key, input Type value
+        public static string GetKeyOfType(Type t)
+        {//look through all the keys in the dictionary
+           foreach (string key in _ShapeClassRegistry.Keys)
+            {//if type matches then output the key string
+                // else output no key found
+                //getting value of dictionary using key
+
+
+                //You can access individual key / value pair of the Dictionary by using its index value.Here, you just specify the key in the index to get the value from the given dictionary
+
+                //link: https://www.geeksforgeeks.org/c-sharp-dictionary-with-examples/
+
+
+                if (t== _ShapeClassRegistry[key])
+                {
+                    return key;
+                }
+            }
+            return "No Key Found for the type";
+            
+            
+        }
+
+        public static Shape CreateShape(string name)
+        {
+            return (Shape)Activator.CreateInstance(_ShapeClassRegistry[name]);
+        }
+
+        public Boolean selected
+        {
+            get { return _selected; }
+            set { _selected = value; }
+        }
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public abstract void Draw();
+
+        public abstract Boolean IsAt(Point2D pt);
+
+
+        public abstract void DrawOutline();
+        public virtual void SaveTo( StreamWriter writer)
+        {   //how to call the type for all the child classes?
+
+            //shouldnt the child follow the the command in the virtual method
+            //then see "this" keyword and thus put its own shape type (like rectangle) and thus
+            //provide the key?
+
+            writer.WriteLine(GetKeyOfType(this.GetType()));
+
+            writer.WriteColor(_color);
+            writer.WriteLine(X);
+            writer.WriteLine(Y);
+        }
+
+        public virtual void LoadFrom(StreamReader reader)
+        {
+            Color = reader.ReadColor();
+            X = reader.ReadInteger();
+            Y = reader.ReadInteger();
+            
+        }
+
+    }
+}
