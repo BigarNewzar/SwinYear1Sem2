89b6882070f3a3729962e25bffcae55ccec49653
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 17:51:48 +0600

    Add files via upload

---
+++ cos20007  stuff/week1 stuff/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Message myMessage;
+            myMessage = new Message("Hello World - from Message Object");
+            myMessage.Print();
+
+            //Console.ReadLine();
+
+            Message[] messages = new Message[4];
+            messages[0] = new Message("Good name. Ever heard of Chris Pratt? ");
+            messages[1] = new Message("Way too common name. Care to get a new one?");
+            messages[2] = new Message("Bro, dont break the 4th wall just for a name test.");
+            messages[3] = new Message("That is a silly name");
+
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            
+            if (name.ToLower() == "chris")
+            {
+                messages[0].Print();
+
+            }
+
+            else if (name.ToLower() == "andrew")
+            {
+                messages[1].Print();
+
+            }
+
+            else if (name.ToLower() == "deadpool")
+            {
+                messages[2].Print();
+
+            }
+
+            else
+            {
+                messages[3].Print();
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
