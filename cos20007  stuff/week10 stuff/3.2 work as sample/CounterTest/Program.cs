fa8f877c4345908df3bce7377038fcf7bcfb0fa2
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 16:30:17 +0600

    Add files via upload

---
+++ cos20007  stuff/week10 stuff/3.2 work as sample/CounterTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading; //namespace to be able to use the threading for delay
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterTest
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+
+            for (int i=0; i<24*60*60 ;i++)
+            {
+                myClock.tick();
+
+                
+                Thread.Sleep(1000);//waits 1000 miliseconds before running the loop again. 
+                                //need to use namespace system thread or else it wont recognise it!
+
+                Console.WriteLine(myClock.PrintTime());
+
+                
+            }
+        } 
+    }
+}
