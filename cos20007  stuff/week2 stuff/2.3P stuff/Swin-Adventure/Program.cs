8a62ece594b9c4c71395510b44a4d55b67bf92e3
BigarNewzar <78624110+BigarNewzar@users.noreply.github.com>
2021-11-12 17:43:46 +0600

    Add files via upload

---
+++ cos20007  stuff/week2 stuff/2.3P stuff/Swin-Adventure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Swin_Adventure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IdentifiableObject id =
+    new IdentifiableObject(new string[] { "ID1", "id2" });
+
+            id.AddIdenfier("test");
+
+            if (id.AreYou("test")==true) { Console.WriteLine("ok"); }
+        }
+    }
+}
